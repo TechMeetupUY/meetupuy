5400836c93564071c288a50b2977c33c4308d3bb
Diego Sapriza <diego@sapriza.com>
2012-09-04 20:23:47 -0300

    cuando y mapa

--- public/contact.php
+++ public/contact.php
@@ -1,50 +1,18 @@
 <!DOCTYPE html>
 <html lang="en">
   <head>
-    <meta charset="utf-8">
-    <title>Bounce | Contact</title>
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta name="description" content="">
-    <meta name="author" content="Evan Clemente">
-    <link href="css/bootstrap.css" rel="stylesheet">
-	<link href="css/font-awesome.css" rel="stylesheet">
-	<link href="css/fonts.css" rel="stylesheet">
-    <link href="css/style.css" rel="stylesheet">
-    <!--[if lt IE 9]>
-      <script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
-    <![endif]-->
-    <link rel="shortcut icon" href="img/favicon.ico">
-    <link rel="apple-touch-icon-precomposed" sizes="144x144" href="img/ico/apple-touch-icon-144-precomposed.png">
-    <link rel="apple-touch-icon-precomposed" sizes="114x114" href="img/ico/apple-touch-icon-114-precomposed.png">
-    <link rel="apple-touch-icon-precomposed" sizes="72x72" href="img/ico/apple-touch-icon-72-precomposed.png">
-    <link rel="apple-touch-icon-precomposed" href="img/ico/apple-touch-icon-57-precomposed.png">
+   <?php require_once("head_meta.php"); ?>
   </head>
 
   <body>
 
-    <div class="navbar navbar-fixed-top">
-      <div class="navbar-inner">
-        <div class="container">
-          <a class="brand" href="index.html">Bounce</a>
-          <ul class="nav pull-right">
-            <li><a href="index.html">Home</a></li>
-			<li><a href="alt.html">Alt. Home</a></li>
-            <li><a href="about.html">About</a></li>
-			<li><a href="features.html">Features</a></li>
-			<li><a href="pricing.html">Pricing</a></li>
-			<li><a data-toggle="modal" href="#joinModal">Register</a></li>
-			<li><a data-toggle="modal" href="#loginModal">Login</a></li>
-            <li class="active"><a href="contact.html">Contact</a></li>
-          </ul>
-        </div>
-      </div>
-    </div>	
+    <?php require_once("navbar.php"); ?>
 	
 	<div id="header">
 	  <div class="container">  
 	    <div class="row">
 		  <div class="span12">
-		    <h1>Contact Us</h1>
+		    <h1>Contacto</h1>
 		  </div>
 		</div>
 	  </div>
@@ -55,159 +23,56 @@
 	    <div class="row">
 		  <div class="span5">
             <h2><i class="icon-envelope"></i> Email Us</h2>		  
-	    <fieldset>
-		  <div class="control-group">
-		    <div class="controls">
-			  <input class="span5" type="text" placeholder="Your Full Name">
-			</div>
-		    <div class="controls">
-			  <input class="span5" type="text" placeholder="Your Email">
-			</div>			
-		    <div class="controls">
-			  <input class="span5" type="text" placeholder="Message Subject">
-			</div>
-			<div class="controls">
-			  <textarea class="span5" id="textarea" rows="6" placeholder="Your Message"></textarea>
-		    </div>
-		  <button class="btn btn-primary">Send Message</button>
-		  </div>	
-	    </fieldset>
+	    	<fieldset>
+			  <div class="control-group">
+			    <div class="controls">
+				  <input class="span5" type="text" placeholder="Your Full Name">
+				</div>
+			    <div class="controls">
+				  <input class="span5" type="text" placeholder="Your Email">
+				</div>			
+			    <div class="controls">
+				  <input class="span5" type="text" placeholder="Message Subject">
+				</div>
+				<div class="controls">
+				  <textarea class="span5" id="textarea" rows="6" placeholder="Your Message"></textarea>
+			    </div>
+			  <button class="btn btn-primary">Send Message</button>
+			  </div>	
+		    </fieldset>
 		  </div>
 		  <div class="span7">
 		    <div class="row">
 		      <div class="span3">
-		        <h2><i class="icon-map-marker"></i> Address</h2>
+		        <h2><i class="icon-map-marker"></i> Dónde</h2>
 		        <address>
-		          111 Some Street, Suite 100
+		          Torre de las Telecomunicaciones
 		          <br>
-		          San Francisco, CA 94107
+		          Av. Rondeau y Guatemala
+		          <br>
+		          Montevideo, UY
 				</address>
 		      </div>
 		      <div class="span4">
-		        <h2><i class="icon-phone"></i> Phone</h2>
-		        <address>
-		          Phone: (123) 456-7890
-		          <br>
-		          Fax: (123) 456-7890 	
-				</address>
-		      </div>			  	   
-		    </div>		  
-			<div class="thumbnail">
-			  <img border="0" alt="" src="http://maps.googleapis.com/maps/api/staticmap?center=Brooklyn+Bridge,New+York,NY&zoom=14&size=540x230&maptype=roadmap&sensor=false">
-			</div>  
+		      	<h2><i class="icon-calendar"></i> Cuándo</h2>
+		      	<p>
+		      		3 de Noviembre de 2012
+		      	</p>
+		      </div>
+		    </div>	
+		    <div class="row">
+		    	<div class="span7">
+		    		<iframe width="500" height="200" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://maps.google.com/maps?f=q&amp;source=s_q&amp;hl=es-419&amp;geocode=&amp;q=Torre+de+las+Telecomunicaciones,+Montevideo,+Uruguay&amp;aq=0&amp;oq=Torre+de+las+Telecomunicaciones,+M+Uruguay&amp;sll=-34.092758,-54.494162&amp;sspn=0.041155,0.077162&amp;ie=UTF8&amp;hq=&amp;hnear=Auditorio+Torre+de+las+Telecomunicaciones,+Guatemala+1075,+Montevideo+11800,+Uruguay&amp;t=m&amp;ll=-34.892126,-56.194382&amp;spn=0.016192,0.046263&amp;z=14&amp;iwloc=A&amp;output=embed"></iframe><br /><small><a href="https://maps.google.com/maps?f=q&amp;source=embed&amp;hl=es-419&amp;geocode=&amp;q=Torre+de+las+Telecomunicaciones,+Montevideo,+Uruguay&amp;aq=0&amp;oq=Torre+de+las+Telecomunicaciones,+M+Uruguay&amp;sll=-34.092758,-54.494162&amp;sspn=0.041155,0.077162&amp;ie=UTF8&amp;hq=&amp;hnear=Auditorio+Torre+de+las+Telecomunicaciones,+Guatemala+1075,+Montevideo+11800,+Uruguay&amp;t=m&amp;ll=-34.892126,-56.194382&amp;spn=0.016192,0.046263&amp;z=14&amp;iwloc=A" style="color:#0000FF;text-align:left">Ver mapa más grande</a></small>
+		    	</div>
+		    </div>	  
 		  </div>		  
 		</div>	
 	  </div>
 	</div>
 
-	<div id="footer">
-	  <div class="container">
-	    <div class="row">
-		  <div class="span3">
-			<h3>Quick Links</h3>
-			<ul>
-			  <li><a href="#">Home</a></li>
-			  <li><a href="#">About Us</a></li>
-			  <li><a href="#">Features</a></li>
-			  <li><a href="#">Pricing</a></li>
-			</ul>			
-		  </div>
-		  <div class="span3">		
-			<h3>Company</h3>
-			<ul>
-			  <li><a href="#">Privacy Policy</a></li>
-			  <li><a href="#">Terms of Use</a></li>
-			  <li><a href="#">FAQ</a></li>
-			</ul>	
-		  </div>
-		  <div class="span3">
-			<h3>We're Social</h3>
-			<ul>
-			  <li><a href="#">Facebook</a></li>
-			  <li><a href="#">Twitter</a></li>
-			  <li><a href="#">Google &#43;</a></li>
-			</ul>		  
-		  </div>
-		  <div class="span3">
-			<h3>Subscribe</h3>
-			<p>Subscribe to our newsletter and stay up to date with the latest news and deals!</p>			
-		    <form>
-              <input type="text" placeholder="Your Email" name="subscribe">
-			  <button class="btn btn-primary">Subscribe</button>
-			</form>
-		  </div>		  
-		</div>	  
-      </div>	  
-    </div>	
 
-	<div id="copywrite">
-	  <div class="container">
-	    <div class="row">
-		  <div class="span12">
-		    <p>&copy; 2012 Bounce Template <span id="totop" class="pull-right">Back to Top <i class="icon-arrow-up"></i></span></p>			
-		  </div>		  
-		</div>	  
-      </div>	  
-    </div>
-
-    <div class="modal hide fade" id="joinModal">
-      <div class="modal-header">
-        <button type="button" class="close" data-dismiss="modal"><i class="icon-remove"></i></button>
-        <h3>Become a Bounce Member</h3>
-      </div>
-      <div class="modal-body">
-	    <fieldset>
-		  <div class="control-group">
-		    <div class="controls">
-			  <label>First Name:</label>
-			  <input class="span3" type="text" placeholder="Your First Name">
-			</div>
-		    <div class="controls">
-			  <label>Last Name:</label>
-			  <input class="span3" type="text" placeholder="Your Last Name">
-			</div>			
-		    <div class="controls">
-			  <label>Email Address:</label>
-			  <input class="span3" type="text" placeholder="Your Email">
-			</div>
-			<div class="controls">
-			  <label>Password:</label>
-			  <input class="span3" type="text" placeholder="Your Password">
-		    </div>			
-		  </div>	
-	    </fieldset>
-      </div>
-      <div class="modal-footer">
-		<button class="btn btn-primary">Bounce Me</button>
-      </div>
-    </div>	
-	
-    <div class="modal hide fade" id="loginModal">
-      <div class="modal-header">
-        <button type="button" class="close" data-dismiss="modal"><i class="icon-remove"></i></button>
-        <h3>Login to Bounce</h3>
-      </div>
-      <div class="modal-body">
-	    <fieldset>
-		  <div class="control-group">
-		    <div class="controls">
-			  <label>Email Address:</label>
-			  <input class="span3" type="text" placeholder="Your Email">
-			</div>
-			<div class="controls">
-			  <label>Password:</label>
-			  <input class="span3" type="text" placeholder="Your Password">
-		    </div>
-			<div class="controls">
-			  <a class="btn btn-warning lefty">Forgot Your Password?</a>  
-		    </div>			
-		  </div>	
-	    </fieldset>
-      </div>
-      <div class="modal-footer">
-		<button class="btn btn-primary">Log In</button>
-      </div>
-    </div>	
+	<?php require_once("footer.php"); ?>
+    <?php require_once("modals.php"); ?>
 
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js"></script>
 	<script>window.jQuery || document.write('<script src="js/jquery-1.7.2.min.js"><\/script>')</script>
