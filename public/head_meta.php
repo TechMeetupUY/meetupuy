ebe925117dfabc4614c8b00ae9b778dd1a0afdc8
Diego Sapriza <diego@sapriza.com>
2012-09-13 15:33:49 -0300

    google favicon

--- public/head_meta.php
+++ public/head_meta.php
@@ -8,9 +8,10 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="description" content="">
     <link href="css/bootstrap.css" rel="stylesheet">
-        <link href="css/font-awesome.css" rel="stylesheet">
-        <link href="css/fonts.css" rel="stylesheet">
+    <link href="css/font-awesome.css" rel="stylesheet">
+    <link href="css/fonts.css" rel="stylesheet">
     <link href="css/style.css" rel="stylesheet">
+    <link rel="shortcut icon" href="favicon.ico">
     <!--[if lt IE 9]>
       <script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
     <![endif]-->
