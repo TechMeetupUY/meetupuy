6587bbec3cb80a378a3a5169bb4cf81c2767c3da
Diego Sapriza <diego@sapriza.com>
2012-10-03 18:22:00 -0300

    saque case

--- public/index.php
+++ public/index.php
@@ -9,13 +9,13 @@
         <?php require_once("navbar.php"); ?>
 
         <div id="landing">
-            <div class="container">  
+            <div class="container">
                 <div class="row">
                     <div class="span10 offset1" style="text-align:center;">
                         <p>
                             <img src="/img/tech-meetup-logo-big.png">
                         </p>
-                        <h1>Bienvenidos a la primer conferencia para desarrolladores en Uruguay.</h1>	
+                        <h1>Bienvenidos a la primer conferencia para desarrolladores en Uruguay.</h1>
 
 <!-- <p><a href="#" class="btn btn-primary btn-action">Get Started Now</a></p>  -->
                     </div>
@@ -36,7 +36,7 @@
                             <i class="icon-user"></i>
                         </div>
                         <h2>Audiencia</h2>
-                        <p>Todos aquellos interesados en las últimas tecnologías del mundo de desarrolladores.</p>	
+                        <p>Todos aquellos interesados en las últimas tecnologías del mundo de desarrolladores.</p>
                     </div>
                     <div class="span3 bouncy">
                         <div class="icon_wrapper">
@@ -47,30 +47,30 @@
                             <br>
                             Av. Rondeau y Guatemala
                             <br>
-                            Montevideo, UY</p>	
+                            Montevideo, UY</p>
                     </div>
                     <div class="span3 bouncy">
                         <div class="icon_wrapper">
                             <i class="icon-calendar"></i>
                         </div>
                         <h2>Cuándo</h2>
-                        <p>Sábado, 3 de Noviembre de 2012</p>	
-                        <p>Inicio 9:00am</p>	
+                        <p>Sábado, 3 de Noviembre de 2012</p>
+                        <p>Inicio 9:00am</p>
                     </div>
                     <div class="span3 bouncy">
                         <div class="icon_wrapper">
                             <i class="icon-money"></i>
                         </div>
                         <h2>Cuánto</h2>
-                        <p>Precio único $U 400.-</p>	
+                        <p>Precio único $U 400.-</p>
                     </div>
                 </div>
-                <hr class="dashed">			
+                <hr class="dashed">
                 <div class="row">
                     <div class="span12">
                         <h2>Algunos de los temas que trataremos:</h2>
                     </div>
-                </div>	
+                </div>
                 <div class="row sponsors">
                     <div class="span4">
                         <div class="lista-temas">
@@ -88,7 +88,7 @@
                             </ul>
                         </div>
                     </div>
-                    
+
                     <div class="span4">
                         <div class="lista-temas">
                             <ul class="unstyled plans">
@@ -104,7 +104,7 @@
                             </ul>
                         </div>
                     </div>
-                    
+
                     <div class="span4">
                         <div class="lista-temas">
                             <ul class="unstyled plans">
@@ -117,45 +117,47 @@
                                 <li class="tema">Experiencias reales de todo lo anterior junto!</li>
                             </ul>
                         </div>
-                    </div>	
+                    </div>
                 </div>
 
-                <hr class="dashed">			
+                <hr class="dashed">
                 <div class="row">
                     <div class="span12">
                         <h2>Sponsors</h2>
                     </div>
-                </div>	
+                </div>
                 <div class="row sponsors">
                     <div class="span4">
                         <a class="thumbnail" href="http://guruhub.com.uy/?ref=meetup.uy" target="_blank">
                             <img alt="image" src="/img/sponsors/guruhub.png">
-                        </a>		
+                        </a>
                     </div>
                     <div class="span4">
                         <a class="thumbnail" href="http://centra.com.uy/?ref=meetup.uy" target="_blank">
                             <img alt="image" src="/img/sponsors/centra.jpg">
                         </a>
                     </div>
+                    <!--
                     <div class="span4">
                         <a class="thumbnail" href="http://case-inc.com/?ref=meetup.uy" target="_blank">
                             <img alt="image" src="/img/sponsors/case.png">
-                        </a>		
+                        </a>
                     </div>
-                    
+
                 </div>
                 <div class="row sponsors">
+                -->
                     <div class="span4">
                         <a class="thumbnail" href="/sponsors">
                             <img alt="image" src="http://placehold.it/300x100&text=Apoyá!!">
                         </a>
-                    </div>	
+                    </div>
                 </div>
                 <div class="row">
                     <div class="span12">
-                        <hr class="dashed">				
-                    </div>  
-                </div> 
+                        <hr class="dashed">
+                    </div>
+                </div>
 
             </div>
         </div>
@@ -167,7 +169,7 @@
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js"></script>
     <script>window.jQuery || document.write('<script src="js/jquery-1.7.2.min.js"><\/script>')</script>
     <script src="/js/bootstrap.js"></script>
-    <script src="/js/script.js"></script>	
+    <script src="/js/script.js"></script>
 
 </body>
 </html>
