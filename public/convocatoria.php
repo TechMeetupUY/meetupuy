52afd5cd892ad248d829bbee2140e83c53bae57b
Diego Sapriza <diego@sapriza.com>
2012-10-09 00:51:51 -0200

    registro

--- public/convocatoria.php
+++ public/convocatoria.php
@@ -9,19 +9,19 @@
     <?php require_once("navbar.php"); ?>
 
 	<div id="header">
-	  <div class="container">  
+	  <div class="container">
 	    <div class="row">
 		  <div class="span12">
-		    <h1>Convocatoria</h1>
+		    <h1>Registrate!</h1>
 		  </div>
 		</div>
 	  </div>
 	</div>
 
 	<div id="content">
-	  <div class="container">			
+	  <div class="container">
 	    <div class="row">
-		  <div class="span7">	
+		  <div class="span7">
 			<p>
 				<strong>techMeetup.uy</strong> es el primer evento que expone la capacidad de las últimas tecnologías a la comunidad de desarrollo uruguaya en sus diversas especialidades.
 			</p>
@@ -39,19 +39,19 @@
 	  	  	<?php require_once 'cuadro_speakers.php'; ?>
 	  	  </div>
 		</div>
-	   
+
         <div class="row">
 		  <div class="span12">
-		  	<hr class="dashed">	
-			<h2 class="txt-middle">Preinscribite!</h2>			
-		  </div>  
-		</div>		
+		  	<hr class="dashed">
+			<h2 class="txt-middle">Llena este formulario para inscribirte.</h2>
+		  </div>
+		</div>
 	    <div class="row">
 		  <div class="span8 offset2">
-		  <iframe src="https://docs.google.com/a/meetup.uy/spreadsheet/embeddedform?formkey=dGNaVWdQdEoyT2Q3bXBtNlFRejE0Z1E6MQ" 
+		  <iframe src="https://docs.google.com/a/meetup.uy/spreadsheet/embeddedform?formkey=dGNaVWdQdEoyT2Q3bXBtNlFRejE0Z1E6MQ"
 		  width="600" height="1300" frameborder="0" marginheight="0" marginwidth="0">Cargando...</iframe>
 		  </div>
-		</div>		
+		</div>
 	  </div>
 	</div>
 
@@ -62,6 +62,6 @@
 	<script>window.jQuery || document.write('<script src="js/jquery-1.7.2.min.js"><\/script>')</script>
     <script src="js/bootstrap.js"></script>
 	<script src="/js/script.js"></script>
-	
+
   </body>
 </html>
