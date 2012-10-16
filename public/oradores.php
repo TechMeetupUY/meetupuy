886044b47a68bcf804e0dd074cc0afeaec541c1b
jorgesierra <georgesierra@gmail.com>
2012-10-16 12:32:58 -0200

    beta ribbon

--- public/oradores.php
+++ public/oradores.php
@@ -5,7 +5,11 @@
   </head>
 
   <body>
-
+    <div class="github-fork-ribbon-wrapper right">
+        <div class="github-fork-ribbon">
+            <a href="#">Beta</a>
+        </div>
+    </div>
     <?php require_once("navbar.php"); ?>
 
 	<div id="header">
