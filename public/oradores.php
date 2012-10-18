89d524c9a05145e3ecc124bacf6854e2f789dda1
Diego Sapriza <diego@sapriza.com>
2012-10-18 19:03:58 -0200

    varios

--- public/oradores.php
+++ public/oradores.php
@@ -24,21 +24,21 @@
 
                     <div class="span4">
                         <div class="thumbnail">
-                            <a data-toggle="modal" href="#ledesmaModal"><img alt="image" src="/img/oradores/ledesma_square.jpg"></a>
+                            <a data-toggle="modal" href="#ledesma"><img alt="image" src="/img/oradores/ledesma_square.jpg"></a>
                         </div>
-                        <h2 class="sub">Gabriel Ledesma <a data-toggle="modal" href="#ledesmaModal"><span class="icon-plus" data-original-title="Leer más"></a></span></h2>
+                        <h2 class="sub">Gabriel Ledesma <a data-toggle="modal" href="#ledesma"><span class="icon-plus" data-original-title="Leer más"></a></span></h2>
                     </div>
                     <div class="span4">
                         <div class="thumbnail">
-                            <a data-toggle="modal" href="#balinoModal"><img alt="image" src="/img/oradores/enriqueb_square.jpg"></a>
+                            <a data-toggle="modal" href="#balino"><img alt="image" src="/img/oradores/enriqueb_square.jpg"></a>
                         </div>
-                        <h2 class="sub">Enrique Baliño <a data-toggle="modal" href="#balinoModal"><span class="icon-plus" data-original-title="Leer más"></span></a></h2>
+                        <h2 class="sub">Enrique Baliño <a data-toggle="modal" href="#balino"><span class="icon-plus" data-original-title="Leer más"></span></a></h2>
                     </div>
                     <div class="span4">
                         <div class="thumbnail">
-                            <a data-toggle="modal" href="#silvaModal"><img alt="image" src="/img/oradores/fede_silva.jpg"></a>
+                            <a data-toggle="modal" href="#silva"><img alt="image" src="/img/oradores/fede_silva.jpg"></a>
                         </div>
-                        <h2 class="sub">Fede Silva <a data-toggle="modal" href="#silvaModal"><span class="icon-plus" data-original-title="Leer más"></a></span></h2>
+                        <h2 class="sub">Fede Silva <a data-toggle="modal" href="#silva"><span class="icon-plus" data-original-title="Leer más"></a></span></h2>
                     </div>
                 </div>
 
@@ -47,67 +47,67 @@
                 <div class="row">
                     <div class="span4">
                         <div class="thumbnail badge-inside">
-                            <a data-toggle="modal" href="#morenoModal"><img alt="image" src="/img/oradores/sebastian_moreno.jpg"></a>
+                            <a data-toggle="modal" href="#moreno"><img alt="image" src="/img/oradores/sebastian_moreno.jpg"></a>
                         </div>
-                        <h2 class="sub">Sebastián Moreno <a data-toggle="modal" href="#morenoModal"><span class="icon-plus" data-original-title="Leer más"></a></h2>
+                        <h2 class="sub">Sebastián Moreno <a data-toggle="modal" href="#moreno"><span class="icon-plus" data-original-title="Leer más"></a></h2>
                     </div>
                     <div class="span4">
                         <div class="thumbnail badge-inside">
                             <!--<span class="badge green">Beta</span> -->
-                            <a data-toggle="modal" href="#martinezModal"><img alt="image" src="/img/oradores/maximo_martinez.jpg"></a>
+                            <a data-toggle="modal" href="#martinez"><img alt="image" src="/img/oradores/maximo_martinez.jpg"></a>
                         </div>
-                        <h2 class="sub">Máximo Martinez <a data-toggle="modal" href="#martinezModal"><span class="icon-plus" data-original-title="Leer más"></a></h2>
+                        <h2 class="sub">Máximo Martinez <a data-toggle="modal" href="#martinez"><span class="icon-plus" data-original-title="Leer más"></a></h2>
                     </div>
                     <div class="span4">
                         <div class="thumbnail badge-inside">
                             <span class="badge orange">Invitado</span>
-                            <a data-toggle="modal" href="#firtmanModal"><img alt="image" src="/img/oradores/maximiliano_firtman.jpg"></a>
+                            <a data-toggle="modal" href="#firtman"><img alt="image" src="/img/oradores/maximiliano_firtman.jpg"></a>
                         </div>
-                        <h2 class="sub">Maximiliano Firtman <a data-toggle="modal" href="#firtmanModal"><span class="icon-plus" data-original-title="Leer más"></a></h2>
+                        <h2 class="sub">Maximiliano Firtman <a data-toggle="modal" href="#firtman"><span class="icon-plus" data-original-title="Leer más"></a></h2>
                     </div>
                 </div>
                 <hr class="dashed">
                 <div class="row">
                     <div class="span4">
                         <div class="thumbnail">
-                            <a data-toggle="modal" href="#iparModal"><img alt="image" src="/img/oradores/fernando_ipar.jpg"></a>
+                            <a data-toggle="modal" href="#ipar"><img alt="image" src="/img/oradores/fernando_ipar.jpg"></a>
                         </div>
-                        <h2 class="sub">Fernando Ipar <a data-toggle="modal" href="#iparModal"><span class="icon-plus" data-original-title="Leer más"></a></h2>
+                        <h2 class="sub">Fernando Ipar <a data-toggle="modal" href="#ipar"><span class="icon-plus" data-original-title="Leer más"></a></h2>
                     </div>
                     <div class="span4">
                         <div class="thumbnail">
-                            <a data-toggle="modal" href="#gomezModal"><img alt="image" src="/img/oradores/adrian.jpg"></a>
+                            <a data-toggle="modal" href="#gomez"><img alt="image" src="/img/oradores/adrian.jpg"></a>
                         </div>
-                        <h2 class="sub">Adrian Gomez <a data-toggle="modal" href="#gomezModal"><span class="icon-plus" data-original-title="Leer más"></a></h2>
+                        <h2 class="sub">Adrian Gomez <a data-toggle="modal" href="#gomez"><span class="icon-plus" data-original-title="Leer más"></a></h2>
                     </div>
                     <div class="span4">
                         <div class="thumbnail">
-                            <a data-toggle="modal" href="#zasModal"><img alt="image" src="/img/oradores/gian.jpg"></a>
+                            <a data-toggle="modal" href="#zas"><img alt="image" src="/img/oradores/gian.jpg"></a>
                         </div>
-                        <h2 class="sub">Gianfranco Zas <a data-toggle="modal" href="#zasModal"><span class="icon-plus" data-original-title="Leer más"></a></h2>
+                        <h2 class="sub">Gianfranco Zas <a data-toggle="modal" href="#zas"><span class="icon-plus" data-original-title="Leer más"></a></h2>
                     </div>
                 </div>
                 <hr class="dashed">
                 <div class="row">
                     <div class="span4">
                         <div class="thumbnail">
-                            <a data-toggle="modal" href="#arrecheModal"><img alt="image" src="/img/oradores/gonzalo.jpg"></a>
+                            <a data-toggle="modal" href="#arreche"><img alt="image" src="/img/oradores/gonzalo.jpg"></a>
                         </div>
-                        <h2 class="sub">Gonzalo Arreche <a data-toggle="modal" href="#arrecheModal"><span class="icon-plus" data-original-title="Leer más"></a></h2>
+                        <h2 class="sub">Gonzalo Arreche <a data-toggle="modal" href="#arreche"><span class="icon-plus" data-original-title="Leer más"></a></h2>
                     </div>
 
                     <div class="span4">
                         <div class="thumbnail">
-                            <a data-toggle="modal" href="#riveraModal"><img alt="image" src="/img/oradores/rodrigo_rivera.png"></a>
+                            <a data-toggle="modal" href="#rivera"><img alt="image" src="/img/oradores/rodrigo_rivera.png"></a>
                         </div>
-                        <h2 class="sub">Rodrigo Rivera <a data-toggle="modal" href="#riveraModal"><span class="icon-plus" data-original-title="Leer más"></a></h2>
+                        <h2 class="sub">Rodrigo Rivera <a data-toggle="modal" href="#rivera"><span class="icon-plus" data-original-title="Leer más"></a></h2>
                     </div>
 
                     <div class="span4">
                         <div class="thumbnail">
-                            <a data-toggle="modal" href="#camargoModal"><img alt="image" src="/img/oradores/gabriel_camargo.jpg"></a>
+                            <a data-toggle="modal" href="#camargo"><img alt="image" src="/img/oradores/gabriel_camargo.jpg"></a>
                         </div>
-                        <h2 class="sub">Gabriel Camargo <a data-toggle="modal" href="#camargoModal"><span class="icon-plus" data-original-title="Leer más"></a></h2>
+                        <h2 class="sub">Gabriel Camargo <a data-toggle="modal" href="#camargo"><span class="icon-plus" data-original-title="Leer más"></a></h2>
                     </div>
                 </div>
                 <!--
@@ -151,6 +151,10 @@
             $(document).ready(function(){
                 var hashes = window.location.href.split("#");
                 $('#'+hashes[1]+'Modal').modal();
+
+                $('a[data-toggle="modal"]').click(function(){
+                    window.location = $(this).attr('href');
+                });
             });
         </script>
     </body>
