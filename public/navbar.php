d73e74820935ca450b36c11e9ed754ef0cf0d39d
Diego Sapriza <diego@sapriza.com>
2012-10-18 16:47:38 -0200

    varios

--- public/navbar.php
+++ public/navbar.php
@@ -4,6 +4,12 @@
  */
 $pagina = $_SERVER["REQUEST_URI"];
 ?>
+<div class="github-fork-ribbon-wrapper right">
+    <div class="github-fork-ribbon">
+        <a href="convocatoria">Registrate</a>
+    </div>
+</div>
+
 <div class="navbar navbar-fixed-top">
       <div class="navbar-inner">
         <div class="container">
@@ -14,12 +20,15 @@ $pagina = $_SERVER["REQUEST_URI"];
             <!--
             <li class="active"><a href="/">Inicio</a></li>
             -->
-            <!-- <li class="<?php if($pagina=='/oradores'){ echo 'active'; }; ?>"><a href="oradores">Oradores</a></li> -->
-            <li class="<?php if($pagina=='/convocatoria'){ echo 'active'; }; ?>"><a href="convocatoria">Registrate!</a></li>
+            <!--
+            <li class="<?php if($pagina=='/oradores'){ echo 'active'; }; ?>"><a href="oradores">Oradores</a></li>
+            <li class="<?php if($pagina=='/agenda'){ echo 'active'; }; ?>"><a href="agenda">Agenda</a></li>
+            -->
+            <li class="<?php if($pagina=='/convocatoria'){ echo 'active'; }; ?>"><a href="convocatoria"><strong>Registrate!</strong></a></li>
             <li class="<?php if($pagina=='/sponsors'){ echo 'active'; }; ?>"><a href="sponsors">Auspicio</a></li>
             <!--<li><a href="inscripcion">Inscripción</a></li>-->
             <li class="<?php if($pagina=='/contact'){ echo 'active'; }; ?>"><a href="contact">Contacto</a></li>
-            <li class="<?php if($pagina=='/organizacion'){ echo 'active'; }; ?>"><a href="organizacion">Organización</a></li>
+            <li class="<?php if($pagina=='/organizacion'){ echo 'active'; }; ?>"><a href="organizacion">Organizan</a></li>
           </ul>
         </div>
       </div>
