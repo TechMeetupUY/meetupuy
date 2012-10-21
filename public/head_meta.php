0fba1387fb6b870cfcb239cb701f90c15ec89253
Diego Sapriza <diego@sapriza.com>
2012-10-21 19:57:16 -0200

    responsive

--- public/head_meta.php
+++ public/head_meta.php
@@ -5,7 +5,7 @@
 ?>
     <meta charset="utf-8">
     <title>Meetup.uy</title>
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta content="width=device-width, initial-scale=1.0" name="viewport">
     <meta name="description" content="Primer conferencia para desarrolladores en Uruguay.">
     <meta name="copyright" content="2012 MeetupUY">
     <link rel="image_src" href="http://meetup.uy/img/tech-meetup-logo-twitter.jpg" />
