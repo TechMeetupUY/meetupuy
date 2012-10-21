4e4599f3485ba001de22328e3fbc185deab7d8a2
Diego Sapriza <diego@sapriza.com>
2012-10-21 19:50:50 -0200

    responsive

--- public/navbar.php
+++ public/navbar.php
@@ -9,13 +9,20 @@ $pagina = $_SERVER["REQUEST_URI"];
         <a href="convocatoria">Registrate</a>
     </div>
 </div>
-
 <div class="navbar navbar-fixed-top">
       <div class="navbar-inner">
         <div class="container">
+            <a data-target=".nav-collapse" data-toggle="collapse" class="btn btn-primary btn-dropnav collapsed">
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </a>
+
           <a class="brand" href="/">
             <img src="/img/tech-meetup-logo-small.png" height="70px" alt=TECHmeetup>
           </a>
+          <div class="nav-collapse collapse">
+
           <ul class="nav pull-right" style="margin-top:20px;">
             <li class="<?php if($pagina=='/oradores'){ echo 'active'; }; ?>"><a href="oradores">Oradores</a></li>
             <!-- <li class="<?php if($pagina=='/agenda'){ echo 'active'; }; ?>"><a href="agenda">Agenda</a></li> -->
@@ -25,6 +32,7 @@ $pagina = $_SERVER["REQUEST_URI"];
             <li class="<?php if($pagina=='/contact'){ echo 'active'; }; ?>"><a href="contact">Contacto</a></li>
             <li class="<?php if($pagina=='/organizacion'){ echo 'active'; }; ?>"><a href="organizacion">Organizan</a></li>
           </ul>
+          </div>
         </div>
       </div>
     </div>
