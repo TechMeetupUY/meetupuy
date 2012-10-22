20f20853b8f8bd00b263504b4a73245d7140520d
Diego Sapriza <diego@sapriza.com>
2012-10-21 22:20:53 -0200

    include

---
+++ public/include/apoyan.inc.php
@@ -0,0 +1,39 @@
+<?php
+//Empresas que apoyan en la difusion
+?>
+<div class="row">
+    <div class="span12">
+        <h2>Apoyan</h2>
+    </div>
+</div>
+<div class="row apoyan" style="padding-top: 10px;">
+    <div class="span10 offset1">
+        <ul class="thumbnails">
+            <li class="span2">
+                <a class="thumbnail" href="http://cuti.org.uy/?ref=meetup.uy" target="_blank">
+                    <img alt="" src="/img/apoyan/cuti.png">
+                </a>
+            </li>
+            <li class="span2">
+                <a class="thumbnail" href="http://latu21.latu.org.uy/ingenio/" target="_blank">
+                    <img alt="" src="/img/apoyan/ingenio.png">
+                </a>
+            </li>
+            <li class="span2">
+                <a class="thumbnail" href="http://montevideovalley.com/?ref=meetup.uy" target="_blank">
+                    <img alt="" src="/img/apoyan/montevideovalley.png">
+                </a>
+            </li>
+            <li class="span2">
+                <a class="thumbnail" href="http://playavalley.com/?ref=meetup.uy" target="_blank">
+                    <img alt="" src="/img/apoyan/playavalley.png">
+                </a>
+            </li>
+            <li class="span2">
+                <a class="thumbnail" href="http://aje.org.uy/?ref=meetup.uy" target="_blank">
+                    <img alt="" src="/img/apoyan/aje.png">
+                </a>
+            </li>
+        </ul>
+    </div>
+</div>
---
+++ public/include/sponsors.inc.php
@@ -0,0 +1,93 @@
+<?php
+    // Listado de sponsors
+?>
+<div class="row">
+    <div class="span12">
+        <h2>Sponsors</h2>
+    </div>
+</div>
+<div class="row sponsors">
+    <div class="span4">
+        <a class="thumbnail" href="http://guruhub.com.uy/?ref=meetup.uy" target="_blank">
+            <img alt="image" src="/img/sponsors/guruhub.png">
+        </a>
+    </div>
+    <div class="span4">
+        <a class="thumbnail" href="http://centra.com.uy/?ref=meetup.uy" target="_blank">
+            <img alt="image" src="/img/sponsors/centra.jpg">
+        </a>
+    </div>
+    <div class="span4">
+        <a class="thumbnail" href="http://www.moove-it.com/?ref=meetup.uy" target="_blank">
+            <img alt="image" src="/img/sponsors/moove-it.png">
+        </a>
+    </div>
+</div>
+<div class="row sponsors">
+    <div class="span4">
+        <a class="thumbnail" href="http://case-inc.com/?ref=meetup.uy" target="_blank">
+            <img alt="image" src="/img/sponsors/case.png">
+        </a>
+    </div>
+    <div class="span4">
+        <a class="thumbnail" href="http://d11.com.uy/?ref=meetup.uy" target="_blank">
+            <img alt="image" src="/img/sponsors/d11.png">
+        </a>
+    </div>
+    <div class="span4">
+        <a class="thumbnail" href="http://userboss.com/?ref=meetup.uy" target="_blank">
+            <img alt="image" src="/img/sponsors/userboss.jpg">
+        </a>
+    </div>
+</div>
+<div class="row sponsors">
+    <div class="span4">
+        <a class="thumbnail" href="http://newcontext.com/?ref=meetup.uy" target="_blank">
+            <img alt="image" src="/img/sponsors/new-context.png">
+        </a>
+    </div>
+    <div class="span4">
+        <a class="thumbnail" href="http://www.pyxisportal.com/?ref=meetup.uy" target="_blank">
+            <img alt="image" src="/img/sponsors/pyxis.png">
+        </a>
+    </div>
+    <div class="span4">
+        <a class="thumbnail" href="http://farm.co/?ref=meetup.uy" target="_blank">
+            <img alt="image" src="/img/sponsors/farmco.jpg">
+        </a>
+    </div>
+</div>
+<div class="row sponsors">
+    <div class="span4">
+        <a class="thumbnail" href="http://www.anii.org.uy/web/?ref=meetup.uy" target="_blank">
+            <img alt="image" src="/img/sponsors/anii.png">
+        </a>
+    </div>
+    <div class="span4">
+        <a class="thumbnail" href="http://uruit.com/?ref=meetup.uy" target="_blank">
+            <img alt="image" src="/img/sponsors/uruit.png">
+        </a>
+    </div>
+    <div class="span4">
+        <a class="thumbnail" href="http://percona.com/?ref=meetup.uy" target="_blank">
+            <img alt="image" src="/img/sponsors/percona.jpg">
+        </a>
+    </div>
+</div>
+<div class="row sponsors">
+    <div class="span4">
+        <a class="thumbnail" href="http://ingsw.com/?ref=meetup.uy" target="_blank">
+            <img alt="image" src="/img/sponsors/ingsw.png">
+        </a>
+    </div>
+    <div class="span4">
+        <a class="thumbnail" href="http://www.wyeworks.com/?ref=meetup.uy" target="_blank">
+            <img alt="image" src="/img/sponsors/wyeworks.png">
+        </a>
+    </div>
+    <div class="span4">
+        <a class="thumbnail" href="/sponsors">
+            <img alt="image" src="/img/sponsors/apoya.gif">
+        </a>
+    </div>
+</div>
