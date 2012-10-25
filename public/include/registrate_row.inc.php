7b5479eeee3e160b64f60f2dcc689fd20f2f14d4
Diego Sapriza <diego@sapriza.com>
2012-10-25 11:00:20 -0200

    rifa

--- public/include/registrate_row.inc.php
+++ public/include/registrate_row.inc.php
@@ -11,10 +11,10 @@
     </div>
     <div class="span6">
         <div class="callout well">
-            <h2>Si sos estudiante...</h2>
-            <p>y querés una beca, llená el siguiente formulario.</p>
-            <a class="btn btn-primary btn-warning btn-large" href="/convocatoria?beca">
-                Quiero una beca.
+            <h2>Entrada gratis?...</h2>
+            <p>Sortearemos algunas, inscribite...</p>
+            <a class="btn btn-primary btn-warning btn-large" href="https://docs.google.com/a/meetup.uy/spreadsheet/viewform?formkey=dEN1U012VHN4Q3ZCME9vUGt0bmdSVmc6MQ#gid=0" target="_blank">
+                Quiero ganarme una entrada.
                 <i class="icon-arrow-right"></i>
             </a>
         </div>
