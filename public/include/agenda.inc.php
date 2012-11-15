e0193ff55f06506e674c7eb55bce69c3f9520e61
Diego Sapriza <AV4TAr@gmail.com>
2012-11-15 11:27:36 -0200

    init

---
+++ public/include/agenda.inc.php
@@ -0,0 +1,39 @@
+<?php
+$agenda = array(
+    'ci' => array(
+        'titulo' => 'Continuous Improvement - o cómo poner los robots de tu lado.',
+        'descripcion' => '<p>
+                                            <a href="http://inconcertcc.com/" target="_blank">inConcert</a> es una empresa de plaza que produce software de telecomunicaciones. Nuestro último producto es el Social Desktop o "<strong>i6</strong>".
+                                        </p>
+                                        <p>
+                                            i6 está compuesto por varios servicios (17) y una larga lista de proyectos internos (30+).
+                                        </p>
+                                        <p>
+                                            La cantidad interacciones entre la funcionalidad de cada uno de estos módulos y las formas de configurarlos para comprobar calidad, performance, etc nos llevaron a que el equipo implementara un par de "técnicas" interesantes:
+                                        <ul>
+                                            <li>Integración Continua</li>
+                                            <li>(y luego) Entrega continua.</li>
+                                        </ul>
+                                        </p>
+                                        <p>
+                                            La intención de la charla es introducir la problemática y los mecanismos que utilizamos para simplificar el desarrollo, la configuración de sistemas de prueba y de producción y la infraestructura que le da soporte a estas técnicas.
+                                        </p>
+                                        <p>
+                                            Vamos a hablar de los problemas que implementar estas técnicas trae y como se solucionan de la misma manera que se solucionan los problemas de desplegar la aplicación a producción: automatización, integración y entrega continuas.
+                                        </p>
+                                        <p>
+                                            Finalmente vamos a presentar una pequeña demo de como usamos herramientas para automatizar la creación de un pequeño cluster de Jenkins - un server de integración continua.
+                                        </p>',
+        'links' => array(),
+        'oradores' => array(
+            array(
+                'href' => '#silva',
+                'img' => '/img/oradores/fede_silva.jpg',
+            ),
+            array(
+                'href' => '#moreno',
+                'img' => '/img/oradores/sebastian_moreno.jpg',
+            )
+        ),
+    ),
+);
