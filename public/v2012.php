f52153b2f58747025436dc8f1e91922a29967296
Diego Sapriza <AV4TAr@gmail.com>
2012-11-26 16:07:52 -0200

    seba fede

--- public/v2012.php
+++ public/v2012.php
@@ -237,7 +237,7 @@ $title = "edición v2012";
                     </div>
                 </div>
 
-            <div class="row-fluid agenda product-plan">
+                <div class="row-fluid agenda product-plan">
                     <div class="span2 horario">
                         <div class="">
                             <ul class="unstyled">
@@ -256,8 +256,11 @@ $title = "edición v2012";
                         </div>
                     </div>
                     <div class="span6">
+                        <h4> Mr. Pedro Fullstack, a sus órdenes.</h4>
                         <p>
-                            A través de la experiencia de Mr. Pedro, te quiero contar cómo fue incorporando un framework Agile para la gestión de sus procesos dentro de su emprendimiento de software factory, de forma empírica y basado en mejora contínua hizo viable su proyecto sin agregar overhead innecesario en la gestión y por sobre todas las cosas valorando a la gente como su mayor capital de trabajo.
+                            A través de la experiencia de Mr. Pedro, te quiero contar cómo fue incorporando un framework Agile para la gestión de sus procesos dentro de su emprendimiento de software factory,
+                            de forma empírica y basado en mejora contínua hizo viable su proyecto sin agregar overhead innecesario en la gestión y por sobre todas las cosas valorando a la gente como su mayor
+                            capital de trabajo.
                         </p>
                         <p>
                             <i class="icon-tag"></i>
@@ -271,12 +274,72 @@ $title = "edición v2012";
                             <ul class="unstyled">
                                 <li class="bouncy">
                                     <div class="icon_wrapper">
-                                        <a href="http://www.youtube.com/watch?v=w5L5FNzevKs&feature=plcp"><i class="icon-facetime-video"></i></a>
+                                        <a href="http://www.youtube.com/watch?v=w5L5FNzevKs&feature=BFa&list=PLgJwmkWpD_oNKDZJFIJSSsSfUyfzeLZnw"><i class="icon-facetime-video"></i></a>
                                     </div>
                                 </li>
                                 <li class="bouncy">
                                     <div class="icon_wrapper">
-                                        <a href="/docs/v2012/20121103-TECH-meetup-ledesma.pdf" target="_blank"><i class="icon-folder-open"></i></a>
+                                        <a href="/docs/v2012/20121103-TECH-meetup-ledesma.pdf" target="_blank"><i class="icon-th-large"></i></a>
+                                    </div>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="row-fluid agenda product-plan">
+                    <div class="span2 horario">
+                        <div class="">
+                            <ul class="unstyled">
+                                <li>
+                                    <div class="feature">
+                                        <h3>11:45</h3>
+                                    </div>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                    <div class="span3">
+                        <div class="thumbnails">
+                            <!-- <img class="img-rounded" alt="" src="http://placehold.it/260x160"> -->
+                            <img class="img-rounded" alt="" src="/img/v2012/seba_fede.png">
+                        </div>
+                    </div>
+                    <div class="span6 expandable">
+                        <h4>Continuous Improvement - o cómo poner los robots de tu lado.</h4>
+                        <span class="descripcion">
+                        <p>
+                            <a target="_blank" href="http://inconcertcc.com/">inConcert</a> es una empresa de plaza que produce software de telecomunicaciones. Nuestro último producto es el Social Desktop o "<strong>i6</strong>".</p>
+<p> i6 está compuesto por varios servicios (17) y una larga lista de proyectos internos (30+). </p>
+<p> La cantidad interacciones entre la funcionalidad de cada uno de estos módulos y las formas de configurarlos para comprobar calidad, performance, etc nos llevaron a que el equipo implementara un par de "técnicas" interesantes: </p>
+<ul><li>Integración Continua</li><li>(y luego) Entrega continua.</li></ul>
+<p> La intención de la charla es introducir la problemática y los mecanismos que utilizamos para simplificar el desarrollo, la configuración de sistemas de prueba y de producción y la infraestructura que le da soporte a estas técnicas. </p>
+<p> Vamos a hablar de los problemas que implementar estas técnicas trae y como se solucionan de la misma manera que se solucionan los problemas de desplegar la aplicación a producción: automatización, integración y entrega continuas. </p>
+<p> Finalmente vamos a presentar una pequeña demo de como usamos herramientas para automatizar la creación de un pequeño cluster de Jenkins - un server de integración continua. </p>
+                        </span>
+                        <p>
+                            <i class="icon-tag"></i>
+                            <span class="label label-info">agile</span>
+                            <span class="label label-info">scrum</span>
+
+                        </p>
+                    </div>
+                    <div class="span1">
+                        <div>
+                            <ul class="unstyled">
+                                <li class="bouncy">
+                                    <div class="icon_wrapper">
+                                        <a href="http://www.youtube.com/watch?v=1-EHN4oZUCQ&feature=BFa&list=PLgJwmkWpD_oNKDZJFIJSSsSfUyfzeLZnw"><i class="icon-facetime-video"></i></a>
+                                    </div>
+                                </li>
+                                <li class="bouncy">
+                                    <div class="icon_wrapper">
+                                        <a href="https://speakerdeck.com/sebastianmoreno/continuous-improvement-o-como-poner-los-robots-de-tu-lado" target="_blank"><i class="icon-th-large"></i></a>
+                                    </div>
+                                </li>
+                                <li class="bouncy">
+                                    <div class="icon_wrapper">
+                                        <a href="https://github.com/sebastianmoreno/meetup-uy-example" target="_blank"><i class="icon-folder-open"></i></a>
                                     </div>
                                 </li>
                             </ul>
@@ -289,9 +352,10 @@ $title = "edición v2012";
             </div>
         </div>
 
-        <?php require_once 'include/footer.inc.php'; ?>
+
 
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js"></script>
+        <script src="/js/jquery.expander.min.js"></script>
         <script>window.jQuery || document.write('<script src="js/jquery-1.7.2.min.js"><\/script>')</script>
         <script src="js/bootstrap.js"></script>
         <script>
@@ -310,9 +374,18 @@ $title = "edición v2012";
                     function(){
                         $(this).removeClass("popular");
                     }
-            );
+                );
+
+                $('div.expandable span').expander({
+                    slicePoint:       400,  // default is 100
+                    expandPrefix:     ' ', // default is '... '
+                    expandText:       '[...]', // default is 'read more'
+                    //collapseTimer:    5000, // re-collapses after 5 seconds; default is 0, so no re-collapsing
+                    userCollapseText: '[^]'  // default is 'read less'
+                  });
             });
         </script>
 
+        <?php require_once 'include/footer.inc.php'; ?>
     </body>
 </html>
