5b2ee975637e4c8311f6ab2ecb1d1f8174255d2a
Diego Sapriza <AV4TAr@gmail.com>
2012-11-26 15:40:53 -0200

    v2012

--- public/v2012.php
+++ public/v2012.php
@@ -1,5 +1,5 @@
 <?php
-    $title = "Apoyan";
+$title = "edición v2012";
 ?>
 <!DOCTYPE html>
 <html lang="es">
@@ -12,8 +12,8 @@
         <?php require_once 'include/navbar.inc.php'; ?>
 
         <div id="header">
-            <div class="container">
-                <div class="row">
+            <div class="container-fluid">
+                <div class="row-fluid">
                     <div class="span12">
                         <h1>edición v2012</h1>
                     </div>
@@ -26,23 +26,19 @@
                 <div class="row">
                     <div class="span12">
                         <p>
-                            Queremos crear una <strong>conferencia de primera</strong> accesible a la mayor cantidad de desarrolladores posible.
-                            Su apoyo ayudará a pagar los costos locativos y operativos de la misma.
-                            Para hacerlo simple creamos el siguiente paquete. Si tenés alguna idea u otra forma de apoyar mandanos
-                            un <a href="mailto:info@meetup.uy">correo</a>, tenemos flexibilidad y podemos trabajar con ustedes para diseñar
-                            un paquete adecuado.
+
                         </p>
                     </div>
                 </div>
 
-                <div class="row">
+                <div class="row-fluid">
 
                     <div class="span4">
                         <div class="product-plan resumen">
                             <ul class="unstyled plans">
                                 <li>
                                     <div class="feature">
-                                       <h3>3/11/2012</h3>
+                                        <h3>3/11/2012</h3>
                                     </div>
                                 </li>
                                 <li>
@@ -59,7 +55,7 @@
                             <ul class="unstyled plans">
                                 <li>
                                     <div class="feature">
-                                       <h3 style="font-size: 30px;">Torre Telecom. Antel</h3>
+                                        <h3 style="font-size: 30px;">Torre Telecom. Antel</h3>
                                     </div>
                                 </li>
                                 <li>
@@ -76,7 +72,7 @@
                             <ul class="unstyled plans">
                                 <li>
                                     <div class="feature">
-                                       <h3>#meetupUY</h3>
+                                        <h3>#meetupUY</h3>
                                     </div>
                                 </li>
                                 <li>
@@ -90,14 +86,14 @@
 
                 </div>
 
-                <div class="row resumen">
+                <div class="row-fluid resumen">
 
                     <div class="span4">
                         <div class="product-plan resumen">
                             <ul class="unstyled plans">
                                 <li>
                                     <div class="feature">
-                                       <h3 class="corto text-info">245</h3>
+                                        <h3 class="corto text-info">245</h3>
                                     </div>
                                 </li>
                                 <li>
@@ -114,7 +110,7 @@
                             <ul class="unstyled plans">
                                 <li>
                                     <div class="feature">
-                                       <h3 class="corto text-info">30+</h3>
+                                        <h3 class="corto text-info">30+</h3>
                                     </div>
                                 </li>
                                 <li>
@@ -131,7 +127,7 @@
                             <ul class="unstyled plans">
                                 <li>
                                     <div class="feature">
-                                       <h3 class="corto text-info">10+</h3>
+                                        <h3 class="corto text-info">10+</h3>
                                     </div>
                                 </li>
                                 <li>
@@ -144,14 +140,14 @@
                     </div>
 
                 </div>
-                <div class="row resumen">
+                <div class="row-fluid resumen">
 
                     <div class="span4">
                         <div class="product-plan resumen">
                             <ul class="unstyled plans">
                                 <li>
                                     <div class="feature">
-                                       <h3 class="corto text-info">12</h3>
+                                        <h3 class="corto text-info">12</h3>
                                     </div>
                                 </li>
                                 <li>
@@ -168,7 +164,7 @@
                             <ul class="unstyled plans">
                                 <li>
                                     <div class="feature">
-                                       <h3 class="corto text-info">9</h3>
+                                        <h3 class="corto text-info">9</h3>
                                     </div>
                                 </li>
                                 <li>
@@ -185,7 +181,7 @@
                             <ul class="unstyled plans">
                                 <li>
                                     <div class="feature">
-                                       <h3 class="corto text-info">20</h3>
+                                        <h3 class="corto text-info">20</h3>
                                     </div>
                                 </li>
                                 <li>
@@ -199,6 +195,96 @@
 
                 </div>
 
+                <!-- Agenda -->
+                <div class="row-fluid agenda product-plan">
+                    <div class="span2 horario">
+                        <div class="">
+                            <ul class="unstyled">
+                                <li>
+                                    <div class="feature">
+                                        <h3>10:00</h3>
+                                    </div>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                    <div class="span3">
+                        <div class="thumbnails">
+                            <!-- <img class="img-rounded" alt="" src="http://placehold.it/260x160"> -->
+                            <img class="img-rounded" alt="" src="/img/v2012/balino.png">
+                        </div>
+                    </div>
+                    <div class="span6">
+                        <p>
+                            Enrique Baliño es Socio fundador de Xn Consultores. Como consultor de la firma, diseña y facilita la implementación de soluciones de desarrollo ejecutivo y cambio organizacional basado en Indagación Apreciativa.
+                        </p>
+                        <p>
+                            <i class="icon-tag"></i>
+                            <span class="label label-info">motivación</span>
+                            <span class="label label-info">equipos</span>
+                        </p>
+                    </div>
+                    <div class="span1">
+                        <div>
+                            <ul class="unstyled">
+                                <li class="bouncy">
+                                    <div class="icon_wrapper">
+                                        <a href="http://www.youtube.com/watch?v=8gQJq_d2Ezs&feature=BFa&list=PLgJwmkWpD_oNKDZJFIJSSsSfUyfzeLZnw"><i class="icon-facetime-video"></i></a>
+                                    </div>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+
+            <div class="row-fluid agenda product-plan">
+                    <div class="span2 horario">
+                        <div class="">
+                            <ul class="unstyled">
+                                <li>
+                                    <div class="feature">
+                                        <h3>10:45</h3>
+                                    </div>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                    <div class="span3">
+                        <div class="thumbnails">
+                            <!-- <img class="img-rounded" alt="" src="http://placehold.it/260x160"> -->
+                            <img class="img-rounded" alt="" src="/img/v2012/ledesma.png">
+                        </div>
+                    </div>
+                    <div class="span6">
+                        <p>
+                            A través de la experiencia de Mr. Pedro, te quiero contar cómo fue incorporando un framework Agile para la gestión de sus procesos dentro de su emprendimiento de software factory, de forma empírica y basado en mejora contínua hizo viable su proyecto sin agregar overhead innecesario en la gestión y por sobre todas las cosas valorando a la gente como su mayor capital de trabajo.
+                        </p>
+                        <p>
+                            <i class="icon-tag"></i>
+                            <span class="label label-info">agile</span>
+                            <span class="label label-info">scrum</span>
+
+                        </p>
+                    </div>
+                    <div class="span1">
+                        <div>
+                            <ul class="unstyled">
+                                <li class="bouncy">
+                                    <div class="icon_wrapper">
+                                        <a href="http://www.youtube.com/watch?v=w5L5FNzevKs&feature=plcp"><i class="icon-facetime-video"></i></a>
+                                    </div>
+                                </li>
+                                <li class="bouncy">
+                                    <div class="icon_wrapper">
+                                        <a href="/docs/v2012/20121103-TECH-meetup-ledesma.pdf" target="_blank"><i class="icon-folder-open"></i></a>
+                                    </div>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+
+                <hr class="dashed">
 
             </div>
         </div>
@@ -224,7 +310,7 @@
                     function(){
                         $(this).removeClass("popular");
                     }
-                );
+            );
             });
         </script>
 
