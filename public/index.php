2eca8ded430866cd920eea3c19a79fe11993733a
Diego Sapriza <AV4TAr@gmail.com>
2012-11-30 13:36:09 -0200

    v2012

--- public/index.php
+++ public/index.php
@@ -29,7 +29,7 @@
                                     if ($i == 1) {
                                         $active = 'active';
                                     }
-                                    echo '<div class="' . $active . ' item"><a href="https://picasaweb.google.com/103831516229609535032/TechMeetupUY2012#slideshow/5807386998558354130" target="_blank"><img alt="" src="/img/2012_presentacion/tm2012-' . sprintf('%02d', $i) . '.jpg"></a></div>';
+                                    echo '<div class="' . $active . ' item"><a href="/v2012" target="_blank"><img alt="" src="/img/2012_presentacion/tm2012-' . sprintf('%02d', $i) . '.jpg"></a></div>';
                                 }
                                 ?>
                             </div>
