f4d75767c64f1d1f4c8c907051dba2ce462cd798
Diego Sapriza <AV4TAr@gmail.com>
2013-06-05 21:05:42 -0300

    img src

--- public/v2013/index.php
+++ public/v2013/index.php
@@ -6,6 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="description" content="techMeetupUY conferencia por y para desarrolladores de Uruguay">
     <meta name="author" content="Diego Sapriza">
+    <link rel="image_src" href="http://meetup.uy/img/tech-meetup-logo-twitter.jpg" />
 
     <!-- Le styles -->
     <link href="./css/bootstrap.css" rel="stylesheet">
@@ -53,12 +54,7 @@
       <script src="../assets/js/html5shiv.js"></script>
     <![endif]-->
 
-    <!-- Fav and touch icons -->
-    <link rel="apple-touch-icon-precomposed" sizes="144x144" href="../assets/ico/apple-touch-icon-144-precomposed.png">
-    <link rel="apple-touch-icon-precomposed" sizes="114x114" href="../assets/ico/apple-touch-icon-114-precomposed.png">
-      <link rel="apple-touch-icon-precomposed" sizes="72x72" href="../assets/ico/apple-touch-icon-72-precomposed.png">
-                    <link rel="apple-touch-icon-precomposed" href="../assets/ico/apple-touch-icon-57-precomposed.png">
-                                   <link rel="shortcut icon" href="../assets/ico/favicon.png">
+
   </head>
 
   <body>
