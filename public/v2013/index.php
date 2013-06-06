93322dea149907ffd26d673acfdb5f67f6a83a55
Diego Sapriza <AV4TAr@gmail.com>
2013-06-05 21:08:13 -0300

    img src

--- public/v2013/index.php
+++ public/v2013/index.php
@@ -84,13 +84,21 @@
         <p>&copy; techMeetupUY 2013</p>
       </div>
 
-    </div> <!-- /container -->
-
-    <!-- Le javascript
-    ================================================== -->
-    <!-- Placed at the end of the document so the pages load faster -->
+    </div>
     <script src="./js/jquery.js"></script>
+    <script type="text/javascript">
 
+    var _gaq = _gaq || [];
+    _gaq.push(['_setAccount', 'UA-34814216-1']);
+    _gaq.push(['_trackPageview']);
+
+    (function() {
+        var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+        ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+        var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+    })();
+
+</script>
 
   </body>
 </html>
